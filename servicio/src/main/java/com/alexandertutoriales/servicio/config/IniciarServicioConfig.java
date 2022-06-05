cf2ee860f649cba8130ad1a4fddf9035803fecbd
Alexander Fuentes Medina <alexanderfuentes199912@gmail.com>
2022-06-05 18:33:38 -0500

    Se movio el archivo de IniciarServicioConfig a la carpeta config.

--- servicio/src/main/java/com/alexandertutoriales/servicio/config/IniciarServicioConfig.java
+++ servicio/src/main/java/com/alexandertutoriales/servicio/config/IniciarServicioConfig.java
@@ -1,11 +1,11 @@
-package com.alexandertutoriales.servicio.controller;
+package com.alexandertutoriales.servicio.config;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class IniciarServicioController {
+public class IniciarServicioConfig {
     @RequestMapping("/")
     public String iniciarServicio(){
         return "Servicio Iniciado Correctamente";
