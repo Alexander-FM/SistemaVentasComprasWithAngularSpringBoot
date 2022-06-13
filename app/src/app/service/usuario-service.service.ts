6d245b308a003faf3bf6bbddbbb498068b1cef85
Alexander Fuentes Medina <alexanderfuentes199912@gmail.com>
2022-06-12 22:09:15 -0500

    Login implementado con el servicio

---
+++ app/src/app/service/usuario-service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Usuario} from "../entity/Usuario";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  url = 'http://localhost:9090/api/usuario/login';
+
+  /**
+   * Iniciar Sesión, Envía los parámetros de usuario y clave al servicio
+   * @param formData
+   */
+  login(formData: FormData){
+    return this.http.post(this.url, formData);
+  }
+}
